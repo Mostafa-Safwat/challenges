b7b13bfa9ff8f492d10a827ba3ddd721bfcef9d0
Mostafa-Safwat <mostafasafwat404@gmail.com>
2023-09-18 18:08:16 +0300

    removed cdn

---
+++ Form/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./Form/index.html", "./Form/main.js"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
